765efd43f0fb9c1ec53467203a167566ec520440
Denis <dstudenko@gmail.com>
2024-02-15 19:09:23 -0500

    add constrains/validations

--- backend/db/migrations/20240215232550-create-spot.js
+++ backend/db/migrations/20240215232550-create-spot.js
@@ -10,19 +10,26 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       ownerId: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        unique: true
       },
       address: {
-        type: Sequelize.TEXT
+        type: Sequelize.TEXT,
+        allowNull: false,
+        unique: true
       },
       state: {
-        type: Sequelize.TEXT
+        type: Sequelize.TEXT,
+        allowNull: false
       },
       city: {
-        type: Sequelize.TEXT
+        type: Sequelize.TEXT,
+        allowNull: false
       },
       country: {
-        type: Sequelize.TEXT
+        type: Sequelize.TEXT,
+        allowNull: false
       },
       lat: {
         type: Sequelize.DECIMAL
@@ -37,19 +44,22 @@ module.exports = {
         type: Sequelize.TEXT
       },
       price: {
-        type: Sequelize.DECIMAL
+        type: Sequelize.DECIMAL,
+        allowNull: false
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       }
     });
   },
-  async down(queryInterface, Sequelize) {
+  async down(queryInterface, _Sequelize) {
     await queryInterface.dropTable('Spots');
   }
 };
--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -14,16 +14,85 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   Spot.init({
-    ownerId: DataTypes.INTEGER,
-    address: DataTypes.TEXT,
-    state: DataTypes.TEXT,
-    city: DataTypes.TEXT,
-    country: DataTypes.TEXT,
-    lat: DataTypes.DECIMAL,
-    lng: DataTypes.DECIMAL,
-    name: DataTypes.TEXT,
-    description: DataTypes.TEXT,
-    price: DataTypes.DECIMAL
+    ownerId: {
+      type: DataTypes.INTEGER,
+      validate: {
+        isNumeric: true,
+        isInt: true,
+        notNull: true,
+        notEmpty: true
+      }
+    },
+    address: {
+      type: DataTypes.TEXT,
+      validate: {
+        notNull: true,
+        notEmpty: true,
+        len: [5, 80]
+      }
+    },
+    state: {
+      type: DataTypes.TEXT,
+      validate: {
+        isAlpha: true,
+        len: [2, 30],
+        notEmpty: true,
+        notNull: true
+      }
+    },
+    city: {
+      type: DataTypes.TEXT,
+      validate: {
+        isAlpha: true,
+        len: [5, 30],
+        notEmpty: true,
+        notNull: true
+      }
+    },
+    country: {
+      type: DataTypes.TEXT,
+      validate: {
+        isAlpha: true,
+        len: [3, 30],
+        notNull: true,
+        notEmpty: true
+      }
+    },
+    lat: {
+      type: DataTypes.DECIMAL,
+      validate: {
+        isDecimal: true
+      }
+    },
+    lng: {
+      type: DataTypes.DECIMAL,
+      validate: {
+        isDecimal: true
+      }
+    },
+    name: {
+      type: DataTypes.TEXT,
+      validate: {
+        isAlpha: true,
+        len: [3, 50]
+      }
+    },
+    description: {
+      type: DataTypes.TEXT,
+      validate: {
+        notEmpty: true,
+        notNull: true
+      }
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      validate: {
+        notNull: true,
+        notEmpty: true,
+        isNumeric: true,
+        isDecimal: true
+      }
+    }
   }, {
     sequelize,
     modelName: 'Spot',
