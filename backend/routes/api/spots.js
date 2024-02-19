51e388169389bb8a230ab807761ad6a56a8c4142
Denis <dstudenko@gmail.com>
2024-02-19 17:40:44 -0500

    eager load spotImage

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -3,7 +3,11 @@ const router = express.Router();
 const { Spot } = require('../../db/models');
 
 router.get('/', async (req, res, next) => {
-    const spots = await Spot.findAll();
+    const spots = await Spot.findAll({
+        include: {
+            model: SpotImage
+        }
+    });
     return res.json(spots);
 });
 
