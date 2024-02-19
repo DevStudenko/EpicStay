190e4c77c79733250cda65bd620b3f8fa5916f37
Denis <dstudenko@gmail.com>
2024-02-18 21:36:20 -0500

    remove options from user and spot migrations

--- backend/db/migrations/20240215232550-create-spot.js
+++ backend/db/migrations/20240215232550-create-spot.js
@@ -51,7 +51,7 @@ module.exports = {
         allowNull: false,
         type: Sequelize.DATE
       },
-    }, options);
+    });
   },
   async down(queryInterface, Sequelize) {
     await queryInterface.dropTable('Spots');
