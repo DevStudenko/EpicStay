43a0f385e7024a7e3dd84f21048ea9b8ce399b45
Denis <dstudenko@gmail.com>
2024-02-19 21:26:20 -0500

    add imagePreview url to SpotImages

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const { Spot } = require('../../db/models');
+const { SpotImage } = require('../../db/models');
 
 router.get('/', async (req, res, next) => {
-    const spots = await Spot.findAll();
-    return res.json(spots);
+    const spots = await Spot.findAll({
+        include: {
+            model: SpotImage
+        }
+    });
+    let spotObj;
+    spots.forEach(spot => {
+        spotObj = spot.toJSON();
+        spotObj.previewImage = spotObj.SpotImages[0].url;
+        delete spotObj.SpotImages;
+    })
+
+    return res.json(spotObj);
 });
 
 module.exports = router;
